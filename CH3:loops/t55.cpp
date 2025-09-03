af842a552aa3d81bf4311a313bc9dc2ba0377008
Neo <154760820+Neolinks1@users.noreply.github.com>
2025-09-03 16:42:31 +0330

    Update t55.cpp

--- CH3:loops/t55.cpp
+++ CH3:loops/t55.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 int main()
 {
-    int n;
+    int n,max=0;
     cout<<"enter a number:"<<endl;
     for(int i=0;i<7;i++)
     {
         cin>>n;
-        
+    if(n>max)
+         max=n;
     }
+    cout<<"n is big:"<<max<<endl;
 }
